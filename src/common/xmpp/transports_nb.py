e7fe20ec791c2eee983e5268e421ef463043eef6
Santiago Gala <sgala@apache.org>
2008-03-13 21:41:51 +0100

    workaround for pyopenssl problems here.
        TLS method for pyopenssl stalls in my gentoo-amd64
        and breaks the connection most of the time. Either
        I raise an exception to avoid pyopenssl being used
        or I use SSLv23_METHOD instead of TLS1_METHOD. This
        is what this patch does (use OpenSSL.SSL.SSLv23_METHOD)

--- src/common/xmpp/transports_nb.py
+++ src/common/xmpp/transports_nb.py
@@ -741,8 +741,8 @@ class NonBlockingTLS(PlugIn):
 		log.debug("_startSSL_pyOpenSSL called")
 		tcpsock = self._owner.Connection
 		# FIXME: should method be configurable?
-		tcpsock._sslContext = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_METHOD)
-		#tcpsock._sslContext = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv23_METHOD)
+		#tcpsock._sslContext = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_METHOD)
+		tcpsock._sslContext = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv23_METHOD)
 		tcpsock.ssl_errnum = 0
 		tcpsock._sslContext.set_verify(OpenSSL.SSL.VERIFY_PEER, self._ssl_verify_callback)
 		cacerts = os.path.join(gajim.DATA_DIR, 'other', 'cacerts.pem')
